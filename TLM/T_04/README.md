6376fa20efbc31616fb38c4ebed4096d64751e18
André Medeiros <andre.escariao1@gmail.com>
2024-03-23 18:59:54 -0300

    Delete TLM/T_04 directory

--- TLM/T_04/README.md
+++
@@ -1,7 +0,0 @@
-# References:
-
-## Tutorial:
-https://www.doulos.com/knowhow/systemc/tlm-20/tutorial-3-routing-methods-through-interconnect-components/
-
-## EDA Simulator:
-https://www.edaplayground.com/x/6Axq
--- TLM/T_04/run.sh
+++
@@ -1,7 +0,0 @@
-cd /home/runner
-export PATH=/usr/bin:/bin:/tool/pandora64/bin
-export LIBRARY_PATH=:/playground_lib/systemc-2.3.3/lib-linux64
-export LD_LIBRARY_PATH=/playground_lib/systemc-2.3.3/lib-linux64
-export CPATH=:/playground_lib/systemc-2.3.3/include
-export HOME=/home/runner
-g++ -o sim -lsystemc *.cpp  && echo "Compile done. Starting run..." && ./sim  ; echo 'Creating result.zip...' && zip -r /tmp/tmp_zip_file_123play.zip . && mv /tmp/tmp_zip_file_123play.zip result.zip
